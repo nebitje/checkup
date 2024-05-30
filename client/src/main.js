2d045aeeb88bb68556ac2e5cc70df2ac8adea3fb
BuildTools <unconfigured@null.spigotmc.org>
2024-05-30 14:46:55 +0200

    vite init (WERKEND)

---
+++ client/src/main.js
@@ -0,0 +1,15 @@
+const button = document.querySelector("#menu")
+const lijst = document.querySelector(" nav ul")
+lijst.classList.add('hidden');
+
+button.addEventListener('click', function () {
+    this.setAttribute('aria-expanded', this.getAttribute('aria-expanded') === "true" ? "false" : "true")
+    if (lijst.classList.contains('visible')) {
+        lijst.classList.remove('visible')
+        lijst.classList.add('hidden')
+    } else if (lijst.classList.contains('hidden')) {
+        lijst.classList.remove('hidden')
+        lijst.classList.add('visible')
+    }
+    console.log(lijst)
+})
